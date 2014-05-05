0991c356e087f10067b3795cb91d8cedeaa81192
Alpi <Alpieex@gmail.com>
2014-05-05 17:46:29 +0400

    Parameters non static

--- src/main/java/utils/Parameters.java
+++ src/main/java/utils/Parameters.java
@@ -8,7 +8,40 @@ public class Parameters {
     private Parameters() {
     }
 
-    public static final double CUBE_DX = 1000;
-    public static final double CUBE_DY = 1000;
-    public static final double CUBE_DZ = 1000;
+    static Parameters parameters;
+
+    public static Parameters get() {
+        if (parameters == null) {
+            parameters = new Parameters();
+        }
+        return parameters;
+    }
+
+    public double getCUBE_DX() {
+        return CUBE_DX;
+    }
+
+    public void setCUBE_DX(double CUBE_DX) {
+        this.CUBE_DX = CUBE_DX;
+    }
+
+    public double getCUBE_DY() {
+        return CUBE_DY;
+    }
+
+    public void setCUBE_DY(double CUBE_DY) {
+        this.CUBE_DY = CUBE_DY;
+    }
+
+    public double getCUBE_DZ() {
+        return CUBE_DZ;
+    }
+
+    public void setCUBE_DZ(double CUBE_DZ) {
+        this.CUBE_DZ = CUBE_DZ;
+    }
+
+    private double CUBE_DX = 1000;
+    private double CUBE_DY = 1000;
+    private double CUBE_DZ = 1000;
 }
